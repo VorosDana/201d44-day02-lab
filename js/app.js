ac5ac57f12ef6e6840a761f3168cb84680cf6072
Dana Voros <vorosdana@gmail.com>
2019-01-09 15:26:58 -0800

    Built day 3 Javascript, working on html and css

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ if(answer === 'yes' || answer === 'ye' || answer === 'y')
 }
 console.log('numCorrect after second question: ' + numCorrect.toString());
 
-// Reuse answer again
+// Reuse answer again again for question 3
 answer = prompt(user + ', do you think I\'ve lived on the West Coast most of my life?');
 // Make answer lowercase for evaluation
 console.log('User answer 3: ' + answer + ', correct is no');
@@ -71,7 +71,7 @@ if(answer === 'yes' || answer === 'ye' || answer === 'y')
 }
 console.log('numCorrect after third question: ' + numCorrect.toString());
 
-// Reuse answer again
+// Reuse answer again again for question 4
 answer = prompt('Here\'s a fun one, ' + user + ', did I recently get my first video game console?');
 // Make answer lowercase for evaluation
 console.log('User answer 4: ' + answer + ', correct is no');
@@ -91,7 +91,7 @@ if(answer === 'yes' || answer === 'ye' || answer === 'y')
 }
 console.log('numCorrect after fourth question: ' + numCorrect.toString());
 
-// Reuse answer again
+// Reuse answer again again for question 5
 answer = prompt('One last question, ' + user + ', am I over 30 years old?');
 // Make answer lowercase for evaluation
 console.log('User answer 5: ' + answer + ', correct is no');
@@ -111,12 +111,85 @@ if(answer === 'yes' || answer === 'ye' || answer === 'y')
 }
 console.log('numCorrect after fifth question: ' + numCorrect.toString());
 
+// Get var ready for the guessing game loop
+var guessingAnswerWrong = true;
+// Set number of guesses allowed
+var guessesRemaining = 4;
+
+
+// Generate a number
+var randomNum = 1 + Math.floor(Math.random() * 10);
+// I looked up the method for this on w3 schools
+// https://www.w3schools.com/js/js_random.asp
+
+console.log('Number guessing answer: ' + randomNum.toString());
+
+do{
+    // Reuse answer again for question 6
+    answer = prompt('What number am I thinking of? Guess from 1 to 10. You have four guesses.')
+    // log the user input, and the correct answer
+    console.log('User guessed: ' + answer);
+    console.log('Correct answer: ' + randomNum.toString());
+    console.log('Guesses Remaining: ' + guessesRemaining.toString());
+    //parse the answer once now instead of several times later
+    answer = parseInt(answer);
+
+    if(answer === randomNum)
+    {
+        alert('That\'s right!');
+        numCorrect++;
+        guessingAnswerWrong = false;
+    }else if(answer > randomNum)
+    {
+        alert('That\'s too high, try lower.');
+        guessesRemaining--;
+    }else
+    {
+        alert('That\'s too low, try higher.');
+        guessesRemaining--;
+    }
+} while(guessingAnswerWrong &&  guessesRemaining);
+
+console.log('numCorrect after sixth question: ' + numCorrect.toString());
+
+// Set up array for question 7
+var languagesUsed = ['Latin', 'C', 'C++', 'Java'];
+// Reuse guesses remaining, set to 6
+guessesRemaining = 6;
+
+// Reuse answer again for question 7
+alert('Can you guess a language I\'ve worked with, coding or otherwise? (not counting English, HTML, CSS, or Javascript) You have 6 guesses.');
+console.log('Guesses initialized to: ' + parseInt(guessesRemaining));
+// Initialize guessing answer var
+guessingAnswerWrong = true;
+
+do
+{
+    answer = prompt('What is your guess? (remember, languages are proper nouns)\n You have ' + guessesRemaining.toString() + ' guesses remaining.');
+    console.log('User guessed ' + answer + ' with ' + guessesRemaining.toString() + ' guesses remaining.');
+
+    if(languagesUsed.includes(answer))
+    {
+        alert('That\'s right! I have worked with ' + answer + '!');
+        numCorrect++;
+        guessingAnswerWrong = false;
+    } else
+    {
+        alert('Nope! I haven\'t worked with ' + answer + '!');
+        guessesRemaining--;
+    }
+} while(guessingAnswerWrong && guessesRemaining);
+
+console.log('numCorrect after seventh question: ' + numCorrect.toString());
+
+
+
 // Add up total correct, congratulate if all are correct, 
 // suggest retrying if not
-if(numCorrect === 5)
+if(numCorrect === 7 )
 {
     alert('That\'s great, ' + user + ', you got them all correct!');
 } else
 {
-    alert(user + ', you got ' + numCorrect.toString() + ' out of 5 correct, try again and see if you can get them all!');
+    alert(user + ', you got ' + numCorrect.toString() + ' out of 7 correct, try again and see if you can get them all!');
 }
